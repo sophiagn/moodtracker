73765724775685e0698d8523eb4c2291a2e6844d
Anju Thomas <anjunoblethomas@gmail.com>
2025-02-13 10:58:39 -0500

    Database should only be created if it doesn't already exist now

--- backend.py
+++ backend.py
@@ -6,18 +6,32 @@ connection = sqlite3.connect("emotion.db")
 cursor = connection.cursor()
 
 #Table contains 6 columns, day of week, date, time, emotion intensity level, emotion description, reason
-cursor.execute("create table mood_tracker (day_of_week TEXT, date TEXT, time TEXT, emotion_intensity_level INTEGER, emotion_descriptor TEXT, reason TEXT)")
+#only creating table when it doesn't exist
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS mood_tracker (
+               day_of_week TEXT, 
+               date TEXT, time TEXT, 
+               emotion_intensity_level INTEGER, 
+               emotion_descriptor TEXT, 
+               reason TEXT
+               )
+            """)
 
-mood_sample_values = [
+# checking if table is empty first before inserting sample data
+cursor.execute("SELECT COUNT(*) FROM mood_tracker")
+row_count = cursor.fetchone()[0] # fetching the result of the first column
+
+if row_count == 0: # only inserting the sample data values if the table is empty
+    mood_sample_values = [
     ("Monday", "8-10-2025", "10:00 AM", 1, "Bored", "Nothing to do"),
     ("Friday", "8-14-2025", "6:00 PM", 4, "Excited", "Friends")
-]
-
-cursor.executemany("insert into mood_tracker values(?, ?, ?, ?, ?, ?)", mood_sample_values)
+    ]
+    cursor.executemany("insert into mood_tracker values(?, ?, ?, ?, ?, ?)", mood_sample_values)
 
 for row in cursor.execute("select * from mood_tracker"):
     print(row)
 
+# committing and closing database
 connection.commit()
 connection.close()
 
